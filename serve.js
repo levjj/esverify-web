8390cd77fbd75d05595384d312768626da159a9c
Christopher Schuster <cs@livoris.net>
2017-05-26 22:07:46 -0700

    converted project to jspm

--- serve.js
+++ serve.js
@@ -3,16 +3,20 @@ const express = require('express');
 
 const app = express();
 
+app.use('/try', (req, res, next) => {
+  req.url = req.url + '.html';
+  next('route')
+});
+
 app.use(express.static('build'));
-app.use(express.static('static'));
 app.post('/z3', (req, res) => {
-  const p = spawn('/home/cs/Projects/jsfxs/z3/build/z3', ['-smt2', '-in'], {stdio: ['pipe', 'pipe', 'ignore']});
+  const p = spawn('z3', ['-smt2', '-in'], {stdio: ['pipe', 'pipe', 'ignore']});
   req.pipe(p.stdin);
   p.stdout.pipe(res);
 });
 app.get('/z3', (req, res) => {
-  res.end("Z3Server is running!");
+  res.end("z3 server is running!");
 });
 app.listen(3000, function () {
-  console.log('esverify-browser listening on http://localhost:3000/');
+  console.log('esverify-web listening on http://localhost:3000/');
 })
---
+++ src/examples.js
@@ -0,0 +1,210 @@
+export const examples = {
+  max: `function max(a, b) {
+  requires(typeof(a) === 'number');
+  requires(typeof(b) === 'number');
+  ensures(max(a, b) >= a);
+
+  if (a >= b) {
+    return a;
+  } else {
+    return b;
+  }
+}`,
+  counter: `let counter = 0;
+invariant(typeof counter === 'number');
+invariant(counter >= 0);
+
+function increment() {
+  ensures(counter > old(counter));
+
+  counter++;
+}
+
+function decrement() {
+  ensures(old(counter) > 0 ? counter < old(counter) : counter === old(counter));
+
+  if (counter > 0) counter--;
+}`,
+  sumTo: `function sumTo(n) {
+  requires(typeof n === 'number');
+  requires(n >= 0);
+  ensures(sumTo(n) === (n + 1) * n / 2);
+
+  let i = 0;
+  let s = 0;
+  while (i < n) {
+    invariant(i <= n);
+    invariant(s === (i + 1) * i / 2);
+    i++;
+    s = s + i;
+  }
+  return s;
+}`,
+  inc: `function inc(n) {
+  return n + 1;
+}
+
+let i = 3;
+let j = inc(i);
+assert(j === 4);`,
+  cons: `function cons(x) {
+  function f () { return x; }
+  return f;
+}
+const g = cons(1);
+const g1 = g();
+assert(g1 === 1);
+const h = cons(2);
+const h1 = h();
+assert(h1 === 2);`,
+  f: `let x = 0;
+
+function f() { ensures(pure()); x++; }
+function g() { ensures(pure()); return x + 1; }
+function h1() { /*empty*/ }
+function h2a() { h1(); }
+function h2b() { ensures(pure()); h1(); }
+function h3a() { ensures(pure()); h2a(); }
+function h3b() { ensures(pure()); h2b(); }`,
+  fib: `function fib(n) {
+  ensures(pure());
+
+  if (n <= 1) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+function fibInc(n) {
+  requires(typeof(n) === 'number');
+  requires(n >= 0);
+  ensures(fib(n) >= n);
+  ensures(pure());
+
+  fib(n);
+  if (n >= 2) {
+    fibInc(n - 1); fib(n - 1);
+    fibInc(n - 2); fib(n - 2);
+  }
+}`,
+  twice: `function inc(n) {
+  requires(typeof(n) === 'number');
+  ensures(inc(n) > n);
+
+  return n + 1;
+}
+
+function twice(f, n) {
+  requires(spec(f, x => typeof(x) === 'number', x => f(x) > x));
+  requires(typeof(n) === 'number');
+  ensures(twice(f, n) > n + 1);
+
+  return f(f(n));
+}
+
+const x = 3;
+const y = twice(inc, x);
+assert(y >= 5);`,
+  fMono: `function fib(n) {
+  requires(n >= 0);
+  ensures(pure());
+  ensures(typeof(fib(n)) === 'number');
+
+  if (n <= 1) {
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
+
+function fibInc(n) {
+  requires(n >= 0);
+  ensures(fib(n) <= fib(n + 1));
+  ensures(pure());
+
+  fib(n);
+  fib(n + 1);
+
+  if (n > 0) {
+    fib(n - 1);
+    fibInc(n - 1);
+  }
+
+  if (n > 1) {
+    fib(n - 2);
+    fibInc(n - 2);
+  }
+}
+
+function fMono(f, fInc, n, m) {
+  requires(spec(f, x => x >= 0, x => pure() && typeof(f(x)) === 'number'));
+  requires(spec(fInc, x => x >= 0, x => pure() && f(x) <= f(x + 1)));
+  requires(n >= 0);
+  requires(m >= 0);
+  requires(n < m);
+  ensures(pure());
+  ensures(f(n) <= f(m));
+
+  if (n + 1 === m) {
+    fInc(n);
+  } else {
+    fInc(n);
+    fMono(f, fInc, n + 1, m);
+  }
+}
+
+function fibMono(n, m) {
+  requires(n >= 0);
+  requires(m >= 0);
+  requires(n < m);
+  ensures(pure());
+  ensures(fib(n) <= fib(m));
+
+  fMono(fib, fibInc, n, m);
+}`,
+  mapLen: `class List {
+  constructor(head, tail) { this.head = head; this.tail = tail; }
+  invariant() { return this.tail === null || this.tail instanceof List; }
+}
+
+function map(lst, f) {
+  requires(lst === null || lst instanceof List);
+  requires(spec(f, x => true, x => pure()));
+  ensures(pure());
+  ensures(map(lst, f) === null || map(lst, f) instanceof List);
+
+  if (lst === null) return null;
+  return new List(f(lst.head), map(lst.tail, f));
+}
+
+function len(lst) {
+  requires(lst === null || lst instanceof List);
+  ensures(pure());
+  ensures(len(lst) >= 0);
+
+  return lst === null ? 0 : len(lst.tail) + 1;
+}
+
+function mapLen(lst, f) {
+  requires(spec(f, x => true, x => pure()));
+  requires(lst === null || lst instanceof List);
+  ensures(pure());
+  ensures(len(lst) === len(map(lst, f)));
+
+  const l = len(lst);
+  const r = len(map(lst, f));
+  if (lst === null) {
+    assert(l === 0);
+    assert(r === 0);
+  } else {
+    const l1 = len(lst.tail);
+    assert(l === l1 + 1);
+
+    f(lst.head);
+    const r1 = len(map(lst.tail, f));
+    assert(r === r1 + 1);
+
+    mapLen(lst.tail, f);
+    assert(l1 === r1);
+    assert(l === r);
+  }
+}`
+};
