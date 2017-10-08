e15560629862ecc21f80e1493768d52ae16207d3
Christopher Schuster <cs@livoris.net>
2017-10-08 16:00:32 -0700

    update to esverify 0.3.2

--- src/config.js
+++ src/config.js
@@ -20,15 +20,15 @@ System.config({
     "child_process": "github:jspm/nodelibs-child_process@0.1.0",
     "core-js": "npm:core-js@1.2.7",
     "esprima": "npm:esprima@3.1.3",
-    "esverify": "npm:esverify@0.3.1",
+    "esverify": "npm:esverify@0.3.2",
     "furtive": "npm:furtive@2.3.0",
     "jquery": "npm:jquery@3.2.1",
-    "spectre.css": "npm:spectre.css@0.2.14",
+    "spectre.css": "npm:spectre.css@0.2.15",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
     "github:jspm/nodelibs-buffer@0.1.1": {
-      "buffer": "npm:buffer@5.0.6"
+      "buffer": "npm:buffer@5.0.8"
     },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
@@ -42,7 +42,7 @@ System.config({
     "github:jspm/nodelibs-vm@0.1.0": {
       "vm-browserify": "npm:vm-browserify@0.0.4"
     },
-    "npm:@types/esprima@2.1.33": {
+    "npm:@types/esprima@2.1.35": {
       "@types/estree": "npm:@types/estree@0.0.35"
     },
     "npm:assert@1.4.1": {
@@ -59,8 +59,8 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "w3c-blob": "npm:w3c-blob@0.0.1"
     },
-    "npm:buffer@5.0.6": {
-      "base64-js": "npm:base64-js@1.2.0",
+    "npm:buffer@5.0.8": {
+      "base64-js": "npm:base64-js@1.2.1",
       "ieee754": "npm:ieee754@1.1.8"
     },
     "npm:core-js@1.2.7": {
@@ -73,8 +73,8 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:esverify@0.3.1": {
-      "@types/esprima": "npm:@types/esprima@2.1.33",
+    "npm:esverify@0.3.2": {
+      "@types/esprima": "npm:@types/esprima@2.1.35",
       "@types/estree": "npm:@types/estree@0.0.35",
       "child_process": "github:jspm/nodelibs-child_process@0.1.0",
       "esprima": "npm:esprima@3.1.3",
