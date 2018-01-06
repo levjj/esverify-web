eaf1d832e94ac8b8e8602ca1183bec2d69246b21
Christopher Schuster <cs@livoris.net>
2018-01-06 13:57:51 -0800

    improved max example

--- src/examples.js
+++ src/examples.js
@@ -1,13 +1,16 @@
 export const examples = {
-  max: `function max(a, b) {
+  max: `// This is a live demo, simply edit the code and click "verify" above!
+
+function max(a, b) {
   requires(typeof(a) === 'number');
   requires(typeof(b) === 'number');
-  ensures(res => res >= a); // post-condition does not hold
+  ensures(res => res >= a);
+  ensures(res => res >= b); // this post-condition does not hold
 
-  if (a >= b) {             // due to bug in implementation
-    return b;
-  } else {
+  if (a >= b) {
     return a;
+  } else {
+    return a;               // due to a bug in the implementation
   }
 }`,
   counter: `let counter = 0;
